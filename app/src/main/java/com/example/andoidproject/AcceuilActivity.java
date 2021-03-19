fbb0ce5d2726b42a2007d3c2b1f7e6d13aef7f31
Louis <louis.clerc@etu.univ-grenoble-alpes.fr>
2021-03-19 19:41:57 +0100

    Acceuil et Main, Lancement de l'app

---
+++ app/src/main/java/com/example/andoidproject/AcceuilActivity.java
@@ -0,0 +1,16 @@
+package com.example.andoidproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AcceuilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil);
+    }
+}
