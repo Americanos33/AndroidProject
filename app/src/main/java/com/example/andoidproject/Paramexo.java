8e4e13a3001f7755b250960d5042c131d1270563
D.KOFY <whitebarb.dz@gmail.com>
2021-03-24 13:18:52 +0100

    Paramexo

---
+++ app/src/main/java/com/example/andoidproject/Paramexo.java
@@ -0,0 +1,14 @@
+package com.example.andoidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Paramexo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paramexo);
+    }
+}
